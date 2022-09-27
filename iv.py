83213a8dce1a4b590580b9591dcfe450595ce06d
Syed Inaamul Haq <37792859+Inamulhaq18@users.noreply.github.com>
2022-09-27 11:35:43 +0300

    Update iv.py

--- iv.py
+++ iv.py
@@ -250,6 +250,7 @@ if st.button("Submit"):
                             Product_price=price, Product_image_R_url=links, Product_image_P_url=linksp,user="Abeer",Product_subcategory=sub_cat, variety=varient) 
             st.success("Updated")
             time.sleep(1)
+            
             imageprocessapi(links)#processing the raw image heroku server
             st.write("Image Process Server.....")
             generatedesc(productname_en)
